c914917004d59fcdfe3194c0139c0220568c1cf5
Gaston Caminiti <gastoncaminiti@outlook.com>
2025-05-21 14:15:00 -0300

    Agente ActualizarRotacionManual

--- NavMesh U6/Assets/Scripts/MovimientoPorClick.cs
+++ NavMesh U6/Assets/Scripts/MovimientoPorClick.cs
@@ -4,10 +4,15 @@ using UnityEngine.AI;
 public class MovimientoPorClick : MonoBehaviour
 {
     private NavMeshAgent agente;
-    
+
+    [SerializeField]
+    [Range(3f, 20f)] 
+    private float factorRotacion = 5f; 
     void Start()
     {
         agente = GetComponent<NavMeshAgent>();
+        agente.updateRotation = false;
+        agente.angularSpeed = 0;
     }
 
     void Update()
@@ -20,6 +25,18 @@ public class MovimientoPorClick : MonoBehaviour
             {
                 agente.SetDestination(impacto.point);
             }
+        }
+
+        ActualizarRotacionManual();
+    }
+
+    void ActualizarRotacionManual()
+    {
+        if (agente.remainingDistance > agente.stoppingDistance && agente.velocity.magnitude > 0.1f)
+        {
+            Vector3 direccion = agente.velocity.normalized;
+            Quaternion rotacionDeseada = Quaternion.LookRotation(direccion);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotacionDeseada, Time.deltaTime * factorRotacion);
         }
     }
 }
